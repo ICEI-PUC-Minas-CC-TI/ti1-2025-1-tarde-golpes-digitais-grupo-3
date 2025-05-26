8bdca242801860cd57c6df2af27d55951f03fa7d
Kaique-tec <kaiqu3rezende@gmail.com>
2025-05-26 14:13:20 -0300

    Add files via upload

---
+++ kaique/scripts.js
@@ -0,0 +1,29 @@
+const videos = [
+  {
+    titulo:
+      "HACKERS CONTAM COMO ENGANAM VÍTIMAS DO GOLPE DO PIX; SAIBA COMO SE PROTEGER",
+    url: "https://www.youtube.com/embed/JEP9pmN2GEM?si=sfGveVhk65rev92x",
+  },
+  {
+    titulo: "GOLPES DIGITAIS: INVASÃO DE REDES SOCIAIS É CADA VEZ MAIS COMUM",
+    url: "https://www.youtube.com/embed/G-3jkrjjP7Q?si=USsjIZwpfmO5WcT7",
+  },
+  {
+    titulo: "FRAUDES DIGITAIS CRESCERAM 45% EM 2024",
+    url: "https://www.youtube.com/embed/NkI6j5FeCLY?si=OYxmkRXuSprpwRGM",
+  },
+];
+
+const container = document.getElementById("video-container");
+
+videos.forEach((video) => {
+  const div = document.createElement("div");
+  div.classList.add("video");
+
+  div.innerHTML = `
+    <h4>${video.titulo}</h4>
+    <iframe width="560" height="315" src="${video.url}" frameborder="0" allowfullscreen></iframe>
+  `;
+
+  container.appendChild(div);
+});
