411725dfeda237ab268aacf00434f2276db7f9bd
Bernardo07-av <beaugevi@gmail.com>
2025-06-19 14:48:11 -0300

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -1,38 +1,24 @@
+let likeData = {
+    liked: false,
+    count: 0
+};
 
-let urlDoLink;
+const likeBtn = document.getElementById("like-btn");
+const likeCount = document.getElementById("like-count");
 
-
-fetch('dados.json') 
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Erro ao carregar o JSON');
-    }
-    return response.json();
-  })
-  .then(data => {
-
-    const linkComId1 = data.links.find(link => link.id === 1);
-    
-    if (linkComId1) {
-      urlDoLink = linkComId1.url;
-      console.log('URL do link com id 1:', urlDoLink);
+function updateUI() {
+    if (likeData.liked) {
+        likeBtn.classList.add("liked");
     } else {
-      console.log('Link com id 1 não encontrado');
+        likeBtn.classList.remove("liked");
     }
-  })
-  .catch(error => {
-    console.error('Erro:', error);
-  });
-
-function compareText() {
-    const userText = document.getElementById("userInput").value;
-    const resultElement = document.getElementById("result");
-
-    if (userText.trim() === urlDoLink ) {
-        alert("O link é igual! Ele provavelmente é seguro!");
-    } else {
-        alert("O link é diferente! Pode ser um golpe!");
-    }
+    likeCount.textContent = likeData.count;
 }
 
+likeBtn.addEventListener("click", () => {
+    likeData.liked = !likeData.liked;
+    likeData.count += likeData.liked ? 1 : -1;
+    updateUI();
+});
 
+updateUI();
